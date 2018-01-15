3c05b1b04d8d35d17c2c2dba87bbe22c209a409a
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-01-15 17:25:17 +0800

    rankHistory

--- code/pages/rankHistory/rankHistory.js
+++ code/pages/rankHistory/rankHistory.js
@@ -3,11 +3,21 @@
 
 Page({
     data: {
-        logs: []
+        logs: [],
     },
-    onLoad: function () {
-        this.setData({
 
+    onLoad: function () {
+
+    },
+    onShow:function () {
+        var animation = wx.createAnimation({
+            duration:'3000',
+            timingFunction: 'linear',
+            delay:'400'
         })
+        animation.top(14+'rpx').step()
+        this.setData({
+            animationData:animation.export()
+        });
     }
 })
