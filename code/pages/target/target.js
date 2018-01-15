27dd34491dbf48a71b0f65a884ddb779002fd51e
Ö캺ͦ <409199338@qq.com>
2018-01-15 14:00:01 +0800

    设定目标步数基本完成

--- code/pages/target/target.js
+++ code/pages/target/target.js
@@ -3,7 +3,16 @@
 
 Page({
     data: {
-        logs: []
+        logs: [],
+        list:[0,1,2,3,4],
+        select:0
+    },
+
+    select:function(e){
+      var n = e.currentTarget.dataset.index
+      this.setData({
+        select:n
+      })
     },
     onLoad: function () {
         this.setData({
