2ac07dc11791e969153f704a4ea76b9bd7709fa0
Ö캺ͦ <409199338@qq.com>
2018-01-15 16:04:48 +0800

    成就卡样式

--- code/pages/friendsRank/friendsRank.js
+++ code/pages/friendsRank/friendsRank.js
@@ -7,12 +7,25 @@ Page({
     },
     onLoad: function () {
         this.setData({
+            isShowCard:true,
 
         })
+    },
+
+    showCard:function () {
+        this.setData({
+            isShowCard:true
+        })
+
     },
     goHistory:function () {
         wx.navigateTo({
             url: '../rankHistory/rankHistory'
         })
+    },
+    close:function(){
+      this.setData({
+      isShowCard: false,
+      })
     }
 })
